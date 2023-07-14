a1aa1b0fe6357cfda04e1b5b80f75b5599ec4d4a
Jalolo13 <jalololo13@gmail.com>
2023-07-14 20:21:05 +0600

    end

---
+++ js/modalError.js
@@ -0,0 +1,15 @@
+let modal = document.querySelector(".modal-error");
+let modalBtn = document.querySelectorAll(".inventory-card-btn");
+let close = document.querySelector(".modal-block-close");
+
+modalBtn.forEach((e) => {
+    e.addEventListener("click", () => {
+        modal.style.opacity = 1;
+        modal.style.zIndex = 10;
+    });
+});
+
+close.addEventListener("click", () => {
+    modal.style.opacity = 0;
+    modal.style.zIndex = -1;
+});
